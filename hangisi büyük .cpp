7405295646b2208e9aa6f34202815a7649243de0
Alper Cantekin <128486707+Merigold26@users.noreply.github.com>
2026-05-03 23:47:16 +0300

    Add files via upload

---
+++ hangisi büyük .cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main ()
+
+{
+	int s1,s2,s3,s4,s5;
+	int a1,a=0;
+	scanf("%d\n%d\n%d\n%d\n%d\n",&s1,&s2,&s3,&s4,&s5);
+	a=s1;
+	if ((a<s2))//||(a<a1)) 
+	a=s2;
+	if(a<s3)
+	a=s3;
+	if(a<s4)
+	a=s4;
+	if(a<s5)
+	a=s5;
+	printf("%d",a);
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
